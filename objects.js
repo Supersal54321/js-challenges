1fc6fdf3748bf5fb29e31b7c8f4d7ba408ada72a
Supersal54321 <123377585+Supersal54321@users.noreply.github.com>
2023-01-27 14:35:06 +0000

    added.objects.js

---
+++ objects.js
@@ -0,0 +1,22 @@
+// Object.Property
+// // person.firstName
+// console.log(person.firstName)
+
+// console.log(person["firstName"])
+
+let day = "Monday"
+const person = {
+    homeOwner: "sally",
+    lastName: "Evans",
+    age: 27,
+    occupation: "sales assistant",
+    married: false,
+    homeOwner: false,
+    hobbies: ["tennis", "gardening", "bungee jumping"]
+}
+if (day === "saturday" || day === "sunday"){
+    console.log(`great its ${day}, lets play ${person.hobbies[0]}!`)
+}else{
+    console.log(`oh no! it's a ${day} i hate being a ${person.occupation}`)
+}
+
